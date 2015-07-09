fc8cfe2443af042b0b3ec70cd624e69fd0939ad5
jacegts <jacegts@gmail.com>
2015-07-08 19:36:09 -0600

    ch 15
    ch 15

---
+++ Demos/Email.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Demos_Email : BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MailMessage myMessage = new MailMessage();
+        myMessage.Subject = "Test Message";
+        myMessage.Body = "Hello world, from Planet Wrox";
+        myMessage.From = new MailAddress("jacegts@gmail.com", "Sender Name");
+        myMessage.To.Add(new MailAddress("jace.gts@gmail.com", "Receiver Name"));
+        SmtpClient mySmtpClient = new SmtpClient();
+        mySmtpClient.Send(myMessage);
+    }
+}
